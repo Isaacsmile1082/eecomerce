bdf2e23a3b99219e4f3b89fc16d563cf9216575f
Angel Vazquez <58788162+Isaacsmile1082@users.noreply.github.com>
2022-03-20 17:58:36 -0600

    Config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,20 @@
-import express from 'express';
-
+import express, {Request, Response} from 'express';
 const app = express();
+
 
 app.listen(8000, () => {
   console.log('Listening to port 8000');
+});
+
+app.get('/hello', (req: Request, res: Response) => {
+  console.log('adsa');
+  res.json({
+    ok: 'true',
+  });
+});
+
+app.get('/world', (req: Request, res: Response) => {
+  res.json({
+    ok: 'true',
+  });
 });
